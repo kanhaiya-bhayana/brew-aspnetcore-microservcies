280047f4e74a609afb760d7f9265e8fcd575565c
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-07-22 23:41:02 +0530

    TodoAPI-MS - created

---
+++ TodoAPI/TodoAPI/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI
+{
+    public class TodoDb : DbContext 
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+        {}
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
